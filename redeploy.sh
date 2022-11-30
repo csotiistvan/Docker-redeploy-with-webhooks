0deb71ba43560f10035fd60970197ad86a08b5f4
csotiistvan <109945490+csotiistvan@users.noreply.github.com>
2022-11-30 12:28:02 +0100

    Update redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo docker pull ashcroftt/nginx-test-artwrks:main
+sudo docker pull ashcroftt/web:latest
 sudo docker stop SRV_CONTAINER
 sudo docker system prune -f
-sudo docker run -d --name=SRV_CONTAINER -p 80:80 ashcroftt/nginx-test-artwrks:main
+sudo docker run -d --name=SRV_CONTAINER -p 80:80 ashcroftt/web:latest
