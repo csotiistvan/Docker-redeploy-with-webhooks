9a5e444d80a8f6ae6735d1365247d26cb3efc960
csotiistvan <109945490+csotiistvan@users.noreply.github.com>
2022-12-01 13:44:31 +0100

    Update rejava.sh

--- rejava.sh
+++ rejava.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo docker pull ashcroftt/jar:latestest
+sudo docker pull jutsz/schiele-jar:latest
 sudo docker stop JAR_CONTAINER
 sudo docker system prune -f
-sudo docker run -d --name=JAR_CONTAINER -p 80:80 ashcroftt/jar:latestest
+sudo docker run -d --name=JAR_CONTAINER -p 80:80 jutsz/schiele-jar:latest
