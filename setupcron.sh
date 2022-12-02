d1df4be03bc9505b2671140f3330ea92735dec0a
csotiistvan <109945490+csotiistvan@users.noreply.github.com>
2022-12-02 11:42:36 +0100

    Update setupcron.sh

--- setupcron.sh
+++ setupcron.sh
@@ -1,2 +1,1 @@
-#(crontab -l ; echo "@reboot sudo bash /home/ubuntu/redeploy.sh ") | crontab -;
-(crontab -l ; echo "*/5 * * * * sudo bash /home/ubuntu/redeploy.sh ") | crontab -
+(sudo crontab -l ; echo "@reboot sudo bash /home/ubuntu/redeploy.sh ") | sudo crontab -; (sudo crontab -l ; echo "*/5 * * * * sudo bash /home/ubuntu/redeploy.sh ") | sudo crontab -
