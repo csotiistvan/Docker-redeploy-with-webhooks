174d5eaa973922c6007052a607640b8a1974a3f7
csotiistvan <109945490+csotiistvan@users.noreply.github.com>
2022-12-02 12:39:34 +0100

    Update redeploy.sh
    Updated docker image

--- redeploy.sh
+++ redeploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-message=$(sudo docker pull ashcroftt/web:latestest | egrep '^Status:')
-if [[ $message != "Status: Image is up to date for ashcroftt/web:latestest" ]]
+message=$(sudo docker pull hordodonga/web:latest | egrep '^Status:')
+if [[ $message != "Status: Image is up to date for hordodonga/web:latest" ]]
 then
     sudo docker stop APACHE
     sudo docker system prune -f
-    sudo docker run -d --name=APACHE -p 80:80 ashcroftt/web:latestest
+    sudo docker run -d --name=APACHE -p 80:80 hordodonga/web:latest
     sudo docker start APACHE
 else
     sudo docker start APACHE
