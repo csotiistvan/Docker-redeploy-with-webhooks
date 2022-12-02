557a997b181fbeffecf1121ca34b816c19a9070c
csotiistvan <109945490+csotiistvan@users.noreply.github.com>
2022-12-02 12:04:02 +0100

    Update redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 message=$(sudo docker pull ashcroftt/web:latestest | egrep '^Status:')
-#echo $message
 if [[ $message != "Status: Image is up to date for ashcroftt/web:latestest" ]]
 then
     sudo docker stop APACHE
     sudo docker system prune -f
     sudo docker run -d --name=APACHE -p 80:80 ashcroftt/web:latestest
+    sudo docker start APACHE
+else
     sudo docker start APACHE
 fi
