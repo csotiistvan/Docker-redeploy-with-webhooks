180cc4b044d246a7b6a7a2190fdf2e318d8ce61e
csotiistvan <109945490+csotiistvan@users.noreply.github.com>
2022-12-06 21:28:03 +0100

    Update rejava.sh

--- rejava.sh
+++ rejava.sh
@@ -5,7 +5,7 @@ if [[ $message != "Status: Image is up to date for jutsz/jar:latest" ]]
 then
     sudo docker stop JARJAR
     sudo docker system prune -f
-    sudo docker run -d --name=JARJAR -p 80:8080 -p 5432:5432 jutsz/jar:latest
+    sudo docker run -d --name=JARJAR -p 80:8080 jutsz/jar:latest
     sudo docker start JARJAR
 else
     sudo docker start JARJAR
