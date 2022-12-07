793e056fc844871a5e8cbace4dc4b632fb347f74
csotiistvan <109945490+csotiistvan@users.noreply.github.com>
2022-12-07 18:34:32 +0100

    Update setupcron.sh

--- setupcron.sh
+++ setupcron.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 (sudo crontab -l ; echo "@reboot sudo bash /home/ubuntu/redeploy.sh ") | sudo crontab -        
-#(sudo crontab -l ; echo "@reboot sudo kill -9 `sudo lsof -t -i:9000` ") | sudo crontab -       
-(sudo crontab -l ; echo "@reboot sleep 120 && sudo webhook -hooks /home/ubuntu/hooks.json -hotreload & ") | sudo crontab -   
 (sudo crontab -l ; echo "*/5 * * * * sudo bash /home/ubuntu/redeploy.sh ") | sudo crontab -    
